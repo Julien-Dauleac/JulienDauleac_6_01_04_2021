318973f14922f2591ab6fc6181ac24294b5d5a5c
Julien Dauleac <julien.dauleac@gmail.com>
2021-04-06 15:11:45 +0200

    Setting up the app.js file and middleware

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const sass = require('node-sass');
 
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-})
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-   res.json({message: 'Votre requête a bien été reçue !'});
+app.post('/api/sauces', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+      message: 'Objet crée !'
+  });
+});
+
+app.use('/api/sauces', (req, res, next) => {
+    const sauce = [
+        {
+            _id:'',
+            userId:'',
+            name:'',
+            manufacturer:'',
+            description:'',
+            mainPepper:'',
+            imageUrl:'',
+            heat:'',
+            likes:'',
+            dislikes:'',
+            usersLiked:'',
+            usersDisliked:''
+        },
+];
+    res.status(200).json(sauce)
 });
 
 module.exports = app;
