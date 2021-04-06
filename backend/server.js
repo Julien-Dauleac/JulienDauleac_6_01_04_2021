4e9347515bb8d39400d849799ae2123b058f0c9c
Julien Dauleac <julien.dauleac@gmail.com>
2021-04-06 11:11:24 +0200

    First commit

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((reg, res) => {
-  res.end('Voilà la réponse du serveur !')
-});
-
-server.listen(process.env.PORT || 3000);
--- backend1/server.js
+++ backend1/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((reg, res) => {
-  res.end('Voilà la réponse du serveur !')
+  res.end('Voilà la réponse du serveur !!')
 });
 
 server.listen(process.env.PORT || 3000);
