9173887eb8162fbbd3242f539087c78f8b5a3566
Julien Dauleac <julien.dauleac@gmail.com>
2021-05-12 09:24:26 +0200

    Implementation of different security plugins and modification of .gitignore for the .env file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,9 +6,11 @@ const router = express.Router();
 
 // Importation du controller //
 const userCtrl = require('../controllers/user');
+// Importation du controller de mots de passe //
+const verifyPassword = require('../middleware/verify-password');
 
 // Chiffre le mot de passe de l'utilisateur, ajoute l'utilisateur à la base de données //
-router.post('/signup', userCtrl.signup);
+router.post('/signup', verifyPassword, userCtrl.signup);
 
 // Vérifie les informations d'identification de l'utilisateur,
 // en renvoyant l'identifiant userID depuis la base de données et un TokenWeb JSON signé(contenant également l'identifiant userID)//
